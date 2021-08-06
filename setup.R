ca04cd289d9f4e3ee5cdbda3220c9c803588a908
sithor <sithor@gmail.com>
2021-08-06 17:00:04 +1200

    update

--- setup.R
+++ setup.R
@@ -1,11 +1,14 @@
 if(!require(blogdown)) install.packages("blogdown")
 
 
+## push changes to github
+# git push -f https://github.com/sithor/blogdown main
+
 ## due to bug install updated version of blogdown
 remotes::install_github('rstudio/blogdown')
 blogdown::config_netlify() ## overwrite Netlify 
 blogdown::new_site(theme = "wowchemy/starter-academic")
-
+blogdown::config_Rprofile()
 
 file.edit(".gitignore")
 
